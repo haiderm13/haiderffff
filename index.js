3b24f173d6b1ba928d26d59c96d98f3b1db27dbe
Z <100608116+haiderm13@users.noreply.github.com>
2022-11-11 21:41:05 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+$("body").css("background-color","rgb(49, 22, 75)");
+$(".java").click(function (){
+    $(".ball").css("margin-left","32px");
+    $(".ball").css("background-color","white");
+    $(".java").css("background-color","black");
+    $("body").css("background-color","#161616");
+})
